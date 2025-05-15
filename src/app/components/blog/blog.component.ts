51df9df0331086602410669cd994be317dd08896
Igor Kulebyakin <igor@Igors-MBP-2.lan>
2025-05-15 08:20:35 -0700

    Started implementation. WIP.

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,12 +1,19 @@
+import { ArticleService } from '@/app/services/article/article.service';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ArticleComponent } from '../article/article.component';
+import { of } from 'rxjs';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
     selector: 'app-blog',
     templateUrl: './blog.component.html',
     styleUrls: ['./blog.component.scss'],
-    imports: [MatCardModule]
+    imports: [NgFor, AsyncPipe, MatCardModule, ArticleComponent]
 })
 export class BlogComponent {
-    constructor() {}
+    public articles$ = this.articleService.getArticles();
+    public isAnimationFinished$ = of(true);
+
+    constructor(private readonly articleService: ArticleService) {}
 }
