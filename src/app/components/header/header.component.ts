93715ac0f1ae184fcf76ea1a3c6bfb7ac57fc738
Igor Kulebyakin <igor@Igors-MBP-2.lan>
2025-05-20 23:33:29 -0700

    Fixed all search fields (for all screen sizes). Implemented search request. Now it must be output in terms of retrieved filtered articles (according to the search term) probably on the Blog page. WIP.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
+import { ArticleService } from '@/app/services/article/article.service';
 import { CommonModule } from '@angular/common';
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    Input,
+    OnDestroy,
+    OnInit,
+    signal,
+    ViewChild,
+    ViewEncapsulation
+} from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { Observable } from 'rxjs';
+import { debounceTime, filter, Observable, switchMap } from 'rxjs';
 import { InitializationService } from '../../services/initialization/initialization.service';
 import { SearchFieldComponent } from '../search-field/search-field.component';
 
@@ -26,14 +38,47 @@ import { SearchFieldComponent } from '../search-field/search-field.component';
     encapsulation: ViewEncapsulation.None,
     standalone: true
 })
-export class HeaderComponent {
-    public isOpen = false;
+export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy {
+    public isOpen = signal<boolean>(false);
     public isAnimationFinished$: Observable<boolean> = this.initializationService.isAnimationFinished$;
+    public searchQuery = signal('');
+    private search$ = toObservable(this.searchQuery);
     @Input() isHome?: boolean;
+    @ViewChild('headerToolbarWrapper', { read: ElementRef }) headerToolbarWrapper!: ElementRef<HTMLDivElement>;
 
-    constructor(private readonly initializationService: InitializationService) {}
-    toggleTabletMenu() {
-        this.isOpen = !this.isOpen;
+    constructor(
+        private readonly initializationService: InitializationService,
+        private readonly articleService: ArticleService
+    ) {}
+
+    ngOnInit(): void {
+        this.search$
+            .pipe(
+                debounceTime(500),
+                filter((s) => s.length >= 3),
+                switchMap((s) => {
+                    return this.articleService.getArticles({ entityName: 'article', filterInput: { search: s } });
+                })
+            )
+            .subscribe((response) => {
+                console.log('search response: ', response);
+            });
+    }
+
+    ngAfterViewInit(): void {
+        const observer = new ResizeObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.contentRect.width > 767 && this.isOpen()) {
+                    this.isOpen.set(false);
+                }
+            });
+        });
+
+        observer.observe(this.headerToolbarWrapper.nativeElement);
+    }
+
+    public toggleTabletMenu() {
+        this.isOpen.set(!this.isOpen());
     }
 
     ngOnDestroy(): void {
