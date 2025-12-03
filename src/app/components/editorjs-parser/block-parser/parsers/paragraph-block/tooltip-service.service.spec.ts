99fbdca17c73079f93891f194977fa0925fb19e7
Igor K. <igk19@me.com>
2025-12-03 19:03:39 +0000

    test: fixed one linting error in one of the tests, installed necessary linting package

--- src/app/components/editorjs-parser/block-parser/parsers/paragraph-block/tooltip-service.service.spec.ts
+++ src/app/components/editorjs-parser/block-parser/parsers/paragraph-block/tooltip-service.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import { beforeEach, describe, expect, it } from 'vitest';
 import { TooltipService } from './tooltip-service.service';
 
 describe('TooltipServiceService', () => {
